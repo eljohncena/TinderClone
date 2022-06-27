65c0115112b3d4ad6a691830fc12409cabadb88e
John Chavez <johnchavez@Johns-MBP-2.lan>
2022-06-26 23:55:32 -0400

    Bottom part

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import Header from "./Header";
 import TinderCards from "./TinderCards";
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
 
       <Header />
       <TinderCards />
-      {/* Swipe Buttons */}
+      <SwipeButtons />
 
     </div>
   );
---
+++ tinder-clone/src/SwipeButtons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./SwipeButtons.css";
+import ReplayIcon from "@mui/material/ReplayIcon";
+import CloseIcon from "@mui/material/CloseIcon";
+import StarRateIcon from "@mui/material/StarRateIcon";
+import FavoriteIcon from "@mui/material/FavoriteIcon";
+import FlashOnIcon from "@mui/material/FlashOnIcon";
+import IconButton from "@mui/material/IconButton";
+
+function SwipeButtons() {
+  return (
+    <div className='swipeButtons'></div>
+  )
+}
+
+export default SwipeButtons
