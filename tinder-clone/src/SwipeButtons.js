cd76d4474992bc0dee491f3404174c43bc840fd4
John Chavez <johnchavez@Johns-MBP-2.lan>
2022-06-27 00:33:16 -0400

    adding css to buttons

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import "./SwipeButtons.css";
-import ReplayIcon from "@mui/material/ReplayIcon";
-import CloseIcon from "@mui/material/CloseIcon";
-import StarRateIcon from "@mui/material/StarRateIcon";
-import FavoriteIcon from "@mui/material/FavoriteIcon";
-import FlashOnIcon from "@mui/material/FlashOnIcon";
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarIcon from '@mui/icons-material/Star';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
 import IconButton from "@mui/material/IconButton";
 
 function SwipeButtons() {
-  return (
-    <div className='swipeButtons'></div>
-  )
-}
+    return ( 
+    <div className="swipeButtons">
+        <IconButton className="swipeButtons__repeat"> 
+            <ReplayIcon fontSize="large" />
+        </IconButton>
 
-export default SwipeButtons
+        <IconButton className="swipeButtons__left">
+            <CloseIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton className="swipeButtons__star">
+            <StarIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton className="swipeButtons__right">
+            <FavoriteIcon fontSize="large" />
+        </IconButton>
+
+        <IconButton className="swipeButtons__lightning">
+            <FlashOnIcon fontSize="large" />
+        </IconButton>
+    </div>
+    );
+}
+export default SwipeButtons;
