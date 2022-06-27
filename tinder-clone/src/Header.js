66200c2de1d4a6314acc1d7d3663f22e59365674
John Chavez <johnchavez@Johns-MBP-2.lan>
2022-06-26 22:15:40 -0400

    creating app

---
+++ tinder-clone/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const header = () => {
+  return (
+    <div className='header'>
+        <h2>I am a header</h2>
+    </div>
+  )
+}
+
+export default header
